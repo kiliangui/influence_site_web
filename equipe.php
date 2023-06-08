34ca3bd259a572b9d9f8ae39af7e9a15591b116c
matrix31000 <mathisdaguts@gmail.com>
2023-06-08 08:12:25 +0200

    page equipe

---
+++ equipe.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipe in.fluence</title>
+    <link rel="stylesheet" href="css/style_equipe.css">
+    <?php
+    include "composant/head.html";
+    ?>
+</head>
+<body>
+    
+<?php
+    include "composant/header.html";
+    ?>
+  
+
+<div id="equipe">
+    <h1><strong>L'équipe</strong> derrière in.fluence</h1>
+    
+    <h2>Voici notre équipe:</h2>
+    <ul>
+        <li>
+            <p class="nom">Consultant digital</p>
+            <img src="./img/mathis.jpg" alt="Image 1">
+            <p>Il est responsable de notre <br>design du site web, élabore les <br>stratégies digitales de<br> l’entreprise.<br>
+Mathis Daguts</p>
+        </li>
+        <li>
+            <p  class="nom">PDG</p>
+    <img src="./img/joaquim.jpeg" alt="Image 2">
+            <p>Notre entreprise est dirigée <br>par Joaquim Jorge.</p>
+        </li>
+        <li>
+            <p  class="nom">Directeur artistique</p>
+    <img src="./img/leni_equipe" alt="Image 3">
+            <p>Notre directeur artistique qui<br> s’occupe d’harmoniser les <br>couleurs et le style de notre<br> agence.<br>
+Léni Touzelet</p>
+        </li>
+        <li>
+            <p  class="nom">Développeur web</p>
+    <img src="./img/kilian.jpg" alt="Image 4">
+            <p>C’est notre dev ! Il s’occupe du<br> développement du site ! Il as<br> une collection de nerf composé de 102 pièces.<br>
+Kilian Guilloteaux</p>
+        </li>
+    </ul>
+</div>
+
+
+    
+<?php
+    include "composant/footer.html";
+    ?>
+</body>
+</html>
