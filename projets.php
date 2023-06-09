a51e4a108daa488b86693330f402a0190997f87a
matrix31000 <mathisdaguts@gmail.com>
2023-06-09 11:26:32 +0200

    page projets

--- projets.php
+++ projets.php
@@ -18,12 +18,12 @@
 
         <h1 class="text-center">Nos <strong>projets</strong> originaux !</h1>
         <section id="pro">
-            <div class="proitems"><img src="./img/img1projet.png"><div class="titre"><p>title</p></div></div>
-            <div class="proitems"><img src="./img/img2projet.png"><div class="titre"><p>LA SAUCE </p></div></div>
-            <div class="proitems"><img src="./img/img3projet.png"><div class="titre"><p>POPCORN</p></div></div>
-            <div class="proitems"><img src="./img/img4projet.png"><div class="titre"><p>LE HUB</p></div></div>
-            <div class="proitems"><img src="./img/img5projet.png"><div class="titre"><p>STUDIO DE Mabzouel</p></div></div>
-            <div class="proitems"><img src="./img/img6projet.png"><div class="titre"><p>CRÉATION IDENTITÉ GRAPHIQUE</p></div></div>
+            <div class="proitems"><img src="./img/img1projet.png"><div class="titre"><p>LA SAUCE</p></div></div>
+            <div class="proitems"><img src="./img/img2projet.png"><div class="titre"><p> POPCORN</p></div></div>
+            <div class="proitems"><img src="./img/img3projet.png"><div class="titre"><p>LE HUB</p></div></div>
+            <div class="proitems"><img src="./img/img4projet.png"><div class="titre"><p>STUDIO DE Mabzouel</p></div></div>
+            <div class="proitems"><img src="./img/img5projet.png"><div class="titre"><p>CRÉATION IDENTITÉ GRAPHIQUE</p></div></div>
+            <div class="proitems"><img src="./img/img6projet.png"><div class="titre"><p>LA SAUCE</p></div></div>
         
         </section>
     </main>
